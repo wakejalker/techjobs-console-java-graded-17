09c6a623b559094386bee5ed6b24835a5841834e
jake <32205542+wakejalker@users.noreply.github.com>
2024-04-25 08:29:13 -0500

    completed printJobs() method

--- src/main/java/TechJobs.java
+++ src/main/java/TechJobs.java
@@ -119,7 +119,16 @@ public class TechJobs {
 
     // Print a list of jobs
     private static void printJobs(ArrayList<HashMap<String, String>> someJobs) {
-
-        System.out.println("printJobs is not implemented yet");
+        if (someJobs.size() == 0) {
+            System.out.println("No Results");
+        }
+        for (int i = 0; i < someJobs.size(); i++) {
+            System.out.println();
+            System.out.println("*****");
+            for (Map.Entry<String, String> job : someJobs.get(i).entrySet()) {
+                System.out.println(job.getKey() + ": " + job.getValue());
+            }
+            System.out.println("*****");
+        }
     }
 }
